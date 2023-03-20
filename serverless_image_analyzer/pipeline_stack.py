45dd807da7eee9e93fc6f019ee636e2adaf6e7f9
osataken <osataken@gmail.com>
2023-03-20 14:37:48 +0700

    Add CodePipeline pipeline

---
+++ serverless_image_analyzer/pipeline_stack.py
@@ -0,0 +1,30 @@
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+    aws_codecommit as codecommit,
+    pipelines as pipelines
+)
+
+class ServerlessImageAnalyzerPipelineStack(Stack):
+    def __init__ (self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        repository = codecommit.Repository(
+            self, 'ServerlessImageAnalyzerRepo',
+            repository_name='ServerlessImageAnalyzerRepo'
+        )
+
+        pipeline = pipelines.CodePipeline(
+            self,
+            "Pipeline",
+            synth=pipelines.ShellStep(
+                "Synth",
+                input=pipelines.CodePipelineSource.code_commit(repository, "main"),
+                commands=[
+                    "npm install -g aws-cdk",  # Installs the cdk cli on Codebuild
+                    "pip install -r requirements.txt",  # Instructs Codebuild to install required packages
+                    "cdk synth",
+                ]
+            )
+        )
+
