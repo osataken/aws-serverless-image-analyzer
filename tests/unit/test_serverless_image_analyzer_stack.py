9fe604c707e9cf7da2c95e828ae7bde4b6b8c698
osataken <osataken@gmail.com>
2023-03-21 16:09:38 +0700

    add test cases for infrastructure

--- tests/unit/test_serverless_image_analyzer_stack.py
+++ tests/unit/test_serverless_image_analyzer_stack.py
@@ -1,15 +1,27 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from serverless_image_analyzer.serverless_image_analyzer_stack import ServerlessImageAnalyzerStack
+from infrastructure.serverless_image_analyzer_stack import ServerlessImageAnalyzerStack
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in serverless_image_analyzer/serverless_image_analyzer_stack.py
-def test_sqs_queue_created():
+
+def test_infrastructure_created():
     app = core.App()
     stack = ServerlessImageAnalyzerStack(app, "serverless-image-analyzer")
     template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    template.resource_count_is("AWS::S3::Bucket", 2)
+    template.resource_count_is("AWS::Lambda::Function", 4)
+    template.resource_count_is("AWS::DynamoDB::Table", 1)
+    template.resource_count_is("AWS::CloudFront::Distribution", 1)
+
+
+def test_api_gateway_created():
+    app = core.App()
+    stack = ServerlessImageAnalyzerStack(app, "serverless-image-analyzer")
+    template = assertions.Template.from_stack(stack)
+
+    template.resource_count_is("AWS::ApiGateway::RestApi", 1)
+    template.resource_count_is("AWS::ApiGateway::Resource", 1)
+    template.resource_count_is("AWS::ApiGateway::Method", 4)
+    template.resource_count_is("AWS::ApiGateway::Deployment", 1)
+    template.resource_count_is("AWS::ApiGateway::Stage", 1)
