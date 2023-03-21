5d8577bb8abec93b5918328ddbf0c6e8d907cb8a
osataken <osataken@gmail.com>
2023-03-21 13:20:16 +0700

    format presigned_url.py code

--- src/lambda/presigned_url.py
+++ src/lambda/presigned_url.py
@@ -4,6 +4,7 @@ import logging
 import os
 import uuid
 import json
+
 from botocore.config import Config
 from botocore.exceptions import ClientError
 
@@ -11,26 +12,25 @@ bucket_name = os.environ['IMAGE_UPLOAD_BUCKET']
 runtime_region = os.environ['AWS_REGION']
 
 my_config = Config(
-    signature_version = 's3v4',
-    s3 = {'addressing_style': 'virtual'}
+    signature_version='s3v4',
+    s3={'addressing_style': 'virtual'}
 )
 
 # --------------- Main handler ------------------
 def lambda_handler(event, context):
-
-    s3_client = boto3.client('s3', region_name = runtime_region, config = my_config)
+    s3_client = boto3.client(
+        's3', region_name=runtime_region, config=my_config)
     try:
         file_name = uuid.uuid4().hex + '.jpg'
         response = s3_client.generate_presigned_url('put_object',
                                                     Params={'Bucket': bucket_name,
                                                             'Key': file_name,
-                                                            'ContentType': 'image/jpeg' },
+                                                            'ContentType': 'image/jpeg'},
                                                     ExpiresIn=300)
     except ClientError as e:
         logging.error(e)
         return None
 
-    print(response)
     return {
         "statusCode": 200,
         "headers": {
@@ -38,7 +38,7 @@ def lambda_handler(event, context):
             "Content-Type": "application/json"
         },
         "body": json.dumps({
-            "uploadURL" : response,
-            "Key" : file_name
+            "uploadURL": response,
+            "Key": file_name
         })
     }
